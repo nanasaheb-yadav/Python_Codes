7a0717e04c46be9641746bfbbf3cfa6a7d96dc91
Nanasaheb Yadav <nanayadav33@gmail.com>
2021-07-05 22:13:21 +0530

    Adding SetMismatch.py file

---
+++ SetMismatch.py
@@ -0,0 +1,16 @@
+def findErrorNums(nums):
+    nums = sorted(nums)
+
+    presentSet = set(nums)
+    actualSet = set([i for i in range(1, len(nums) + 1)])
+
+    for i in range(0, len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            result = nums[i]
+
+    diff = min(actualSet.difference(presentSet))
+    return [result, diff]
+
+print(findErrorNums([1,2,3,5,4,6,5]))
+
+#[5, 7]
