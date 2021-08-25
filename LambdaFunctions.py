759d438a70bc8dcbca2f3500ace76f954e294ee8
Nanasaheb Yadav <nanayadav33@gmail.com>
2021-08-25 23:29:00 +0530

    adding JudgeGroup.py file

--- LambdaFunctions.py
+++ LambdaFunctions.py
@@ -1,9 +1,11 @@
 
 
-print((lambda a,b:a if a>b else b)(3,3.5))
+print((lambda a, b: a if a>b else b)(3,3.5))
 
 print((lambda :print("Hi"))())
 
-myfunction = lambda x:x*x*x
+myfunction = lambda x: x*x*x
 a = myfunction(10)
 print(a)
+
+
