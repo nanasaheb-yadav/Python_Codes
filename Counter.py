64bf4cd90e8e46e082cba11d4e8ce2ff46b2d70b
Nanasaheb Yadav <nanayadav33@gmail.com>
2021-09-03 19:08:38 +0530

    updating counter file

--- Counter.py
+++ Counter.py
@@ -9,6 +9,8 @@ def counter(word):
     count = {}
     for letter in word:
         count[letter] = count.get(letter, 0) + 1
+
+    count = {k: v for k, v in sorted(count.items(), key=lambda item: item[1])}
     return count
 
 
