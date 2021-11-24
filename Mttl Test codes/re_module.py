d59bd144aa03efd59fc81ae095efdffc170563d5
Nanasaheb Yadav <nanayadav33@gmail.com>
2021-11-24 22:10:55 +0530

    adding new files

---
+++ Mttl Test codes/re_module.py
@@ -0,0 +1,12 @@
+import re
+
+mydata = " Revolution of the nations, for the nations."
+pattern1 = r'n[a-z]+s'
+pattern2 = r'of\s'
+
+if re.findall(pattern1, mydata):
+    mydata = re.sub(pattern1,'country',mydata)
+if re.findall(pattern2, mydata):
+    mydata = re.sub(pattern2,'by ',mydata)
+
+print(mydata)
