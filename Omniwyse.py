6776d7b76ae4a28685c1285d0b5e05a88d29e456
Nanasaheb Yadav <nanayadav33@gmail.com>
2022-05-27 22:25:42 +0530

    omniwyse interview code

---
+++ Omniwyse.py
@@ -0,0 +1,58 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+ls = [5, 56, 2, 4, 8, 97, 2, 4, 34]
+ls1 = []
+
+for i in ls:
+    if not i % 2 == 0:
+        ls1.append(i)
+
+print(ls1)
+
+print([i for i in ls if not i % 2 == 0])
+
+
+class Omniwyse:
+
+    def __init__(self, roll, name, clas):
+        self.roll = roll
+        self.name = name
+        self.clas = clas
+
+    def details(self):
+        print(f"{self.roll}: {self.name} : {self.clas}")
+
+
+obj = Omniwyse(1, "Student1", "12th")
+obj.details()
+
+select
+roll, name
+from t1 # Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+ls = [5,56,2,4,8,97,2,4,34]
+ls1 = []
+
+for i in ls:
+    if not i %2 ==0:
+        ls1.append(i)
+
+print(ls1)
+
+print([i for i in ls if not i%2==0])
+
+class Omniwyse:
+
+    def __init__(self, roll, name,clas):
+        self.roll = roll
+        self.name = name
+        self.clas = clas
+
+    def details(self):
+        print(f"{self.roll}: {self.name} : {self.clas}")
+
+obj = Omniwyse(1,"Student1", "12th")
+obj.details()
+
