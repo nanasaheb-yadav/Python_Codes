7d9e845128d7c8032a0d99885fd2efdb1f8e25fb
Nanasaheb Yadav <nanayadav33@gmail.com>
2022-09-19 16:21:06 +0530

    adding Gemini.py interview file

---
+++ Gemini.py
@@ -0,0 +1,48 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+#print("Hello world")
+
+num = 12356421
+s = str(num)[::-1]
+print(s)
+
+str1 = "nanasaheb"
+
+count = {}
+for i in str1:
+    count[i]= count.get(i,0)+1
+    
+print(count)
+
+"""list1 = [1,2,3]
+max1 = 0
+for i in range(0,len(list1)):
+    for j in range(1, len(list1)-1):
+        n = list1[i:j]
+        n = [ str(i) for i in n]
+        val= int("".join(n))
+        if val >max1:
+            max1 =val
+print(max1)"""
+
+list1 = [3,2,5,33,6,7,3,2]
+l1 = list1[:len(list1)//2]
+l2 = list1[len(list1)//2:]
+l2.extend(l1)
+list1.extend(l1)
+print(list1)
+
+n= 149 
+14 
+14>9
+1+4 =5
+5>9
+nums =n.split()
+if sum(nums)>nums[-1]
+if sum(nums[-1])>nums[-1]
+for i in range(len(nums)):
+    val = nums[-1]
+    total = sum(nums[:-1])
+    if total > val:
+        
+
