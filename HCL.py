1edc0c78b61bb9e9b5b26be78b52c4e3d4fd2049
Nanasaheb Yadav <nanayadav33@gmail.com>
2022-09-21 11:52:37 +0530

    adding file for HCL

---
+++ HCL.py
@@ -0,0 +1,27 @@
+"""
+HCL Interview- 21 Sept 2022
+1. All OOP Concepts with example
+2. list with unique values from list without set()
+3. create two functions.
+  1st is: divide large number using small number and return divison and remainder without using operator
+  2nd is: add rem and div into smallest number and return that number like below code
+n1 = input("num1")
+n2 = input("num2")
+
+def divide(small,large):
+
+    div = 0
+    rem = 0
+            # div large by small without using any operators
+    return (div,rem)
+
+def valid(n1,n2)
+
+       if n1< n2:
+            div, rem = divide(small=n1,large=n2)
+            largeno = n1+ div+rem
+       else:
+            div, rem = divide(small=n2,large=n1)
+            largeno = n2+div+rem
+      return largeno
+"""
